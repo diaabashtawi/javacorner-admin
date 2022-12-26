fe3ffc5b964a8f8b9b349fa1b2539a26a7b68a56
diaabashtawi <diaabashtawi@gmail.com>
2022-12-26 18:47:41 +0400

    add eun method ro main method

--- src/main/java/com/javacorner/admin/JavacornerAdminApplication.java
+++ src/main/java/com/javacorner/admin/JavacornerAdminApplication.java
@@ -1,6 +1,7 @@
 package com.javacorner.admin;
 
 import com.javacorner.admin.dao.*;
+import com.javacorner.admin.utility.OperationUtility;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -28,6 +29,7 @@ public class JavacornerAdminApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+        OperationUtility.usersOperations(userDao);
 
     }
 }
