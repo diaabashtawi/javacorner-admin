c43ac28f7fa656500afc2492eed1fb23b6f96318
diaabashtawi <diaabashtawi@gmail.com>
2023-02-26 00:40:24 +0400

    Encode password using BCryptPasswordEncoder

--- src/main/java/com/bakheet/service/implementation/UserServiceImpl.java
+++ src/main/java/com/bakheet/service/implementation/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.bakheet.dao.UserDao;
 import com.bakheet.entiy.Role;
 import com.bakheet.entiy.User;
 import com.bakheet.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,9 +17,12 @@ public class UserServiceImpl implements UserService {
 
     private RoleDao roleDao;
 
-    public UserServiceImpl(UserDao userDao, RoleDao roleDao) {
+    private PasswordEncoder passwordEncoder;
+
+    public UserServiceImpl(UserDao userDao, RoleDao roleDao, PasswordEncoder passwordEncoder) {
         this.userDao = userDao;
         this.roleDao = roleDao;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Override
@@ -28,7 +32,10 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User createUser(String email, String password) {
-        return userDao.save(new User(email, password));
+        User user = loadUserByEmail(email);
+        if (user != null) throw new RuntimeException("User with email : " + email + " aleady exist");
+        String encodedPassword = passwordEncoder.encode(password);
+        return userDao.save(new User(email, encodedPassword));
     }
 
     @Override
