260c733ae399ad06a710371ecfdfb4fd0983ba6f
diaabashtawi <diaabashtawi@gmail.com>
2023-02-26 00:27:53 +0400

    create UserDetaile Class

--- src/main/java/com/bakheet/security/SecurityConfiguration.java
+++ src/main/java/com/bakheet/security/SecurityConfiguration.java
@@ -1,16 +1,21 @@
 package com.bakheet.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configurable
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity)throws Exception {
