fdfbe5c61ff98964742c6d5e891d71aac9b90f5e
diaabashtawi <diaabashtawi@gmail.com>
2023-02-25 16:03:19 +0400

    create delete and update method and add student dropdown list to the navbar

--- src/main/java/com/bakheet/controller/StudentController.java
+++ src/main/java/com/bakheet/controller/StudentController.java
@@ -4,6 +4,7 @@ import com.bakheet.entiy.Student;
 import com.bakheet.service.StudentService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -24,13 +25,28 @@ public class StudentController {
         this.studentService = studentService;
     }
 
-    public String studentList(Model model, @RequestParam(name = KEYWORD) String keyword){
-
+    @GetMapping(
+            value = "/list"
+    )
+    public String studentList(Model model, @RequestParam(name = KEYWORD, defaultValue = "") String keyword){
         List<Student> students = studentService.findStudentsByName(keyword);
         model.addAttribute(LIST_STUDENTS, students);
         model.addAttribute(KEYWORD, keyword);
 
         return "student_views/students";
+    }
 
+    @GetMapping(
+            value = "/delete"
+    )
+    public String deleteStudent(Long studentId, String keyword){
+        studentService.removeStudent(studentId);
+        return "redirect:/student/list?keyword=" + keyword;
+    }
+
+    public String updateStudent(Model model, Long studentId){
+        Student student = studentService.loadStudentById(studentId);
+        model.addAttribute("student", student);
+        return "student_views/update";
     }
 }
